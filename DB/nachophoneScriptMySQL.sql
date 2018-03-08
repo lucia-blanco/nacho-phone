580d1b2a6e6b7905fb11b69d0b1693b73eb3fb46
Lucía Blanco <lublasal@gmail.com>
2018-03-08 09:10:20 +0100

    Minor change in syntaxis (again)

--- DB/nachophoneScriptMySQL.sql
+++ DB/nachophoneScriptMySQL.sql
@@ -1,9 +1,10 @@
+CREATE DATABASE `NACHOPHONE` IF NOT EXISTS;
 USE `NACHOPHONE`
 ;
 /****** Object:  Table `GASTO`    Script Date: 07/03/2018 14:20:10 ******/
 
 
-CREATE TABLE `GASTO`(
+CREATE TABLE IF NOT EXISTS`GASTO`(
 	`idGasto` int AUTO_INCREMENT NOT NULL,
 	`usuario` int NULL,
 	`tarifa` int NULL,
@@ -19,7 +20,7 @@ PRIMARY KEY
 /****** Object:  Table `PERFIL`    Script Date: 07/03/2018 14:20:11 ******/
 
 
-CREATE TABLE `PERFIL`(
+CREATE TABLE IF NOT EXISTS `PERFIL`(
 	`idRol` int AUTO_INCREMENT NOT NULL,
 	`rol` varchar(8) NOT NULL,
 	`usuario` int NOT NULL,
@@ -32,7 +33,7 @@ PRIMARY KEY
 /****** Object:  Table `TARIFA`    Script Date: 07/03/2018 14:20:11 ******/
 
 
-CREATE TABLE `TARIFA`(
+CREATE TABLE IF NOT EXISTS `TARIFA`(
 	`idTarifa` int AUTO_INCREMENT NOT NULL,
 	`nomTarifa` varchar(20) NULL,
 	`precio` double NULL,
@@ -47,7 +48,7 @@ PRIMARY KEY
 /****** Object:  Table `USUARIO`    Script Date: 07/03/2018 14:20:11 ******/
 
 
-CREATE TABLE `USUARIO`(
+CREATE TABLE IF NOT EXISTS `USUARIO`(
 	`idUsuario` int AUTO_INCREMENT NOT NULL,
 	`nombre` varchar(50) NOT NULL,
 	`apellido` varchar(50) NOT NULL,
@@ -61,7 +62,6 @@ PRIMARY KEY
 )
 )
 ;
-SET IDENTITY_INSERT `GASTO` ON 
 
 INSERT INTO `GASTO` (`idGasto`, `usuario`, `tarifa`, `telefono`, `mbConsumidos`, `minConsumidos`) VALUES (1, 1, 1, N'111111111', 100, 15)
 , (2, 2, 2, N'222222222', 150, 20)
@@ -70,20 +70,20 @@ INSERT INTO `GASTO` (`idGasto`, `usuario`, `tarifa`, `telefono`, `mbConsumidos`,
 ,(5, 4, 1, N'444444444', 50, 5)
 ,(6, 4, 3, N'444444440', 1500, 800)
 ,(7, 5, 2, N'555555555', 250, 10)
-
+;
 INSERT INTO`PERFIL` (`idRol`, `rol`, `usuario`) VALUES (1, N'admin', 1)
 ,(2, N'admin', 2)
 ,(3, N'usuario', 3)
 ,(4, N'usuario', 4)
 ,(5, N'usuario', 5)
-
+;
 INSERT INTO `TARIFA` (`idTarifa`, `nomTarifa`, `precio`, `mbTarifa`, `minTarifa`) VALUES (1, N'Pato', 5.95, 500, 40)
 ,(2, N'Ganso', 12.84, 1000, 300)
 ,(3, N'Cisne', 21.49, 6000, 1000)
-
+;
 INSERT INTO `USUARIO` (`idUsuario`, `nombre`, `apellido`, `passw`, `dni`, `tlfUsuario`, `email`) VALUES (1, N'Aa', N'Aaa', N'Aa_1', N'11111111A', N'111111111', N'aa@aa.a')
 ,(2, N'Bb', N'Bbb', N'Bb_2', N'22222222B', N'222222222', N'bb@bb.b')
 ,(3, N'Cc', N'Ccc', N'Cc_3', N'33333333C', N'333333333', N'cc@cc.c')
 ,(4, N'Dd', N'Ddd', N'Dd_4', N'44444444D', N'444444444', N'dd@dd.d')
 ,(5, N'Ee', N'Eee', N'Ee_5', N'55555555E', N'555555555', N'ee@ee.e')
-
+;
